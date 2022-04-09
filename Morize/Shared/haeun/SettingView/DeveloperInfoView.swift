ec504474bd22448aaa53bbc7e35a20948daf1e2a
hanni66 <haeunkim0807@naver.com>
2022-04-10 01:40:00 +0900

    Feat: 개발자 정보 디자인 수정

---
+++ Morize/Shared/haeun/SettingView/DeveloperInfoView.swift
@@ -0,0 +1,20 @@
+//
+//  DeveloperInfoView.swift
+//  Morize
+//
+//  Created by 김하은 on 2022/04/10.
+//
+
+import SwiftUI
+
+struct DeveloperInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DeveloperInfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        DeveloperInfoView()
+    }
+}
